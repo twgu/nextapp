39ab9da20fadd6c02c61374d52c089855b29418a
구태완(home) <>
2023-12-08 09:11:02 +0900

    init

---
+++ readme.txt
@@ -0,0 +1,7 @@
+* 개발환경
+- node.js (v20.10.0)
+- npm install -g npm@10.2.5
+- npx create-next-app@latest .
+
+* 임시 backend 실행
+- npx json-server --port 9999 --watch db.json
---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h2>Welcome</h2>
+      Hello, WEB!
+      <br></br>
+      <img src="/teacher.png" title="teacher" alt="teacher"></img>
+    </>
+  );
+}
